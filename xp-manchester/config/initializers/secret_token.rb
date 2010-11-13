5de10ddfef687f9931def38be527f442e0fe41b1
Jimmeh <jimmeh.mcdonald@gmail.com>
2010-11-13 15:55:53 +0000

    created project

---
+++ xp-manchester/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+XpManchester::Application.config.secret_token = 'cb88bbc4951b60f060188af2baa69e2f2ed4d9c8d633d094553dcb3e59d4425ea49a74dc4a3d9d9fd30b6a4c1f2b2f72b47d830c06328c548ca57e80f4d4a7a3'
