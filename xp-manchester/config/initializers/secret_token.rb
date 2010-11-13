2bdb64279b02f995f56393a3cb909663bf91ca07
Jimmeh <jimmeh.mcdonald@gmail.com>
2010-11-13 15:52:40 +0000

    added project

--- xp-manchester/config/initializers/secret_token.rb
+++ xp-manchester/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-XpManchester::Application.config.secret_token = '6f0c172bf5fa82e51b474d00bbde8ac1476b6eeb43cae16991c79a1793c17d0e927db9c1f6a0c2a0fbfc5dbc60b516b7af1249ff3211cb0c25191e29758e8550'
+XpManchester::Application.config.secret_token = 'b17186b6ccb95020b5ecc9fda5f09c3629dea660abab937fd5eef9013ed950960880627642a52a3790cdf06842fe4f1232e4a75edad981d33cb645be22109b35'
--- xp-manchester/config/routes.rb
+++ xp-manchester/config/routes.rb
@@ -1,6 +1,4 @@
 XpManchester::Application.routes.draw do
-  get "home/index"
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
